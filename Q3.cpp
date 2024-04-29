81bfc1f17e90643c6116d74ce171c82207d030ab
phcarter <phcarter@my.loyno.edu>
2024-04-28 22:36:09 -0500

    q3

--- Q3.cpp
+++ Q3.cpp
@@ -4,7 +4,12 @@
 #include <chrono>
 #include <string>
 #include <cstdlib>
+#include <queue>
 using namespace std;
+
+sem_t empty;
+sem_t full;
+sem_t mutex;
 
 struct requestStructure {
 	int request_id;
@@ -16,22 +21,57 @@ queue<requestStructure> msg_queue;
 
 string webPages[10] = {"google.com", "yahoo.com", "youtube.com", "amazon.com", "facebook.com", "instagram.com", "github.com", "gmail.com", "outlook.com", "chess.com"};
 
-int shared = 0;
+int counter = 0;
 
 void listen() {
-	sleep_for(seconds((rand() % 3) + 1));
-	requestStructure request;
-	shared++;
-	request.request_id = shared;
-	request.ip_address = "";
-	request.page_requested = webPages[rand() % 10]
+	while (true) {
+		sem_wait(&empty);
+		sem_wait(&mutex);
+		std::this_thread::sleep_for(std::chrono::seconds((rand() % 3) + 1));
+		requestStructure request;
+		counter++;
+		request.request_id = counter;
+		request.ip_address = "";
+		int num = rand() % 10;
+		request.page_requested = webPages[num];
+		msg_queue.push(request);
+		sem_post(&mutex);
+		sem_post(&full);
+	}
 }
 
 void do_request() {
-	
+	while (true) {
+		sem_wait(&full);
+		sem_wait(&mutex);
+		if (msg_queue.empty()) {
+			sem_post(&mutex);
+			sem_post(&full);
+			break;
+		}
+		requestStructure request = msg_queue.front();
+		request = msg_queue.front();
+		msg_queue.pop();
+		cout << "thread " << std::this_thread::get_id() << " completed request " << request.request_id << " requesting webpage " << request.page_requested << endl;
+		sem_post(&mutex);
+		sem_post(&full);
+	}	
 }
 
 int main() {
-	
+	// initialize semaphores
+	sem_init(&empty, 0, 10);
+	sem_init(&full, 0, 0);
+	sem_init(&mutex, 0, 1);
+	// create producer and consumer threads
+	thread listen_thread(listen);
+	thread request_thread(do_request);
+	// wait for threads to finish (should never happen)
+	listen_thread.join();
+	request_thread.join();
+	// destroy semaphores
+	sem_destroy(&empty);
+	sem_destroy(&full);
+	sem_destroy(&mutex);
 	return 0;
 }
